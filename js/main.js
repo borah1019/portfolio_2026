8547dd3924193b2c228e010e19f5f01fd000f4a8
이보라 <borah@ibolaui-MacBookPro.local>
2026-06-28 20:36:08 +0900

    2026-06-28

--- js/main.js
+++ js/main.js
@@ -99,3 +99,16 @@ window.addEventListener('load', () => {
 
   document.querySelector('#main .thumbnail').style.height = activeProjectHeight.offsetHeight + 'px';
 })
+
+// PROJECT - swiper
+
+const swiper = new Swiper('.swiper', {
+  // loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
